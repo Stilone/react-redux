0a1df685669537e4798fa2dac5348ee770fedaed
Stilone <54247765+Stilone@users.noreply.github.com>
2021-08-05 12:50:56 +0300

    123
    123

---
+++ src/index.js
@@ -0,0 +1,33 @@
+import {createStore} from "./create-store";
+import './styles.css'
+import {rootReducer} from "./redux/rootReducer";
+
+const counter = document.getElementById('counter');
+const addBtn = document.getElementById('add');
+const subBtn = document.getElementById('sub');
+const asyncBtn = document.getElementById('async');
+const themeBtn = document.getElementById('theme');
+
+const store = createStore(rootReducer, 0)
+
+addBtn.addEventListener('click', () => {
+    store.dispatch({ type: 'INCREMENT' })
+})
+
+subBtn.addEventListener('click', () => {
+    store.dispatch({ type: 'DECREMENT' })
+})
+
+asyncBtn.addEventListener('click', () => {
+
+})
+
+store.subscribe(() => {
+    const state = store.getState()
+    counter.textContent = state
+})
+
+themeBtn.addEventListener('click', () => {
+    // document.body.classList.toggle('dark')
+})
+
