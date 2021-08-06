3905b4fddef68442e3fd8cb6248f79ba26e2133f
Stilone <54247765+Stilone@users.noreply.github.com>
2021-08-06 17:15:03 +0300

    i don't understand a little
    but I'll be better soon:D

--- src/redux/createStore.js
+++ src/redux/createStore.js
@@ -1,3 +1,19 @@
-export const createStore = () => {
+import { InputReducer } from "./inputReducer";
 
+export const Store = (InputReducer) => {
+    let state = {};
+    const subscribes = [];
+
+    return {
+        dispatch(action){
+            state = InputReducer(state, action)
+            
+        },
+        subscribe(callback){
+            subscribes.push(callback)
+        },
+        getStore(state){
+            return state
+        }
+    }
 }
---
+++ src/redux/inputReducer.js
@@ -0,0 +1,3 @@
+export const InputReducer = (action, state) => {
+
+}
